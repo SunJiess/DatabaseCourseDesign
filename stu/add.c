8ec5be628d5c0a6ef3292b955523434a615326fe
SunJ <SunJiess@outlook.com>
2017-06-20 00:30:51 -0700

    Update .c of table  stu->information

--- stu/add.c
+++ stu/add.c
@@ -15,6 +15,8 @@ int cgiMain()
 	char name[32] = "\0";
 	char age[16] = "\0";
 	char stuId[32] = "\0";
+	char sex[16] = "\0";
+	char dept[32] = "\0";
 	int status = 0;
 	char ch;
 
@@ -52,6 +54,20 @@ int cgiMain()
 		return 1;
 	}
 
+	status = cgiFormString("sex",  sex, 16);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get sex error!\n");
+		return 1;
+	}
+
+	status = cgiFormString("dept",  dept, 32);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get dept error!\n");
+		return 1;
+	}
+
 	//fprintf(cgiOut, "name = %s, age = %s, stuId = %s\n", name, age, stuId);
 
 	int ret;
@@ -77,7 +93,7 @@ int cgiMain()
 
 
 
-	strcpy(sql, "create table stu(id int not null primary key, name varchar(20) not null, age int not null)");
+	strcpy(sql, "create table information(id int not null primary key, name varchar(20) not null, age int not null, sex varchar(10) not null,dept varchar(20) not null )");
 	if ((ret = mysql_real_query(db, sql, strlen(sql) + 1)) != 0)
 	{
 		if (ret != 1)
@@ -90,7 +106,7 @@ int cgiMain()
 
 
 
-	sprintf(sql, "insert into stu values(%d, '%s', %d)", atoi(stuId), name, atoi(age));
+	sprintf(sql, "insert into information values(%d, '%s', %d,'%s','%s')", atoi(stuId), name, atoi(age),sex,dept);
 	if (mysql_real_query(db, sql, strlen(sql) + 1) != 0)
 	{
 		fprintf(cgiOut, "%s\n", mysql_error(db));
